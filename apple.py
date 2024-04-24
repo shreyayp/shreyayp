40e374e16bce956c175c00b2ffd7fe79a4ecc1d0
shreyayp <167231166+shreyayp@users.noreply.github.com>
2024-04-24 09:36:57 +0530

    Create apple.py

---
+++ apple.py
@@ -0,0 +1,22 @@
+### Hi there 👋
+
+apple=[10,5,30]
+print(apple)
+print(apple[1])
+print(apple[-1])
+print(apple[-3])
+#print(apple[4])
+print("for iteration via access elements")
+for i in apple:
+    print(i)
+print("After changed")
+apple[1]=-100
+print(apple)
+apple=    
+print(apple)  
+print(apple[-1])
+apple[3]=500,600,700
+print(apple)
+apple[1:4]=(-10,-20,-30)
+print(apple)
+    
