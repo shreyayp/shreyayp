c6d853629de7f18127dd8d20c9cc3bcb9bcc2d95
shreyayp <167231166+shreyayp@users.noreply.github.com>
2024-04-24 09:32:33 +0530

    Create bank.py

---
+++ bank.py
@@ -0,0 +1,24 @@
+
+
+def withdraw(account,amount):
+    if amount>account['balance']:
+        print("Insufficient funds!")
+        else:
+            account['balance'] -= amount
+            
+            account['transactions'].append(f"Withdrawal:${amount}")
+            print(f"Withdrawal successful.Remaining balance:${account['balance']}")
+            
+def deposit(account,amount):
+    account['balance']+=amount
+    account['transactions'].append(f"Deposit:${amount}")
+    print(f"Deposit succcessful.Remaining balance: ${account['balance']}")
+    
+def get_balance(account):
+        return account['balance']
+    
+def get_balance(account):
+    retrun account['balance']
+    
+    def get_transaction_history(account):
+        retrun account['transactions']
